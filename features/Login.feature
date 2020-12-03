0f69fda6875f0fdfe145f3e1c0e7f542d88894b8
Syed Qadri <rqadri133@gmail.com>
2020-12-03 12:15:06 -0500

    Checking expected length of array in Gherkin Ruby

--- features/Login.feature
+++ features/Login.feature
@@ -5,9 +5,9 @@ Scenario: Get User Auth0 Token
    
    """
    { 
-     "client_id": "your_keys", 
-     "client_secret": "your_keys", 
-     "audience": "use_your_own_url",
+     "client_id": "BadElW3sKo6whvAM1UHOEI3WcjkDGZ1w5T6", 
+     "client_secret": "BadZtOKLFyTRwe3GmsIPLJzMBCKn3zXmAlbAiFI85xBtesf2Fe7GOgE6VBwWHov5O3q", 
+     "audience": "https://bluevision.com",
      "grant_type":"client_credentials"
      }
    """
@@ -18,3 +18,10 @@ Scenario: send user id and password in a json body
     Given I send and accept JSON
    
    Then the response status should be "200"
+   
+Scenario: Load all buyers 
+  Given i send data for buyers information and accept  data in JSON
+
+  Then the buyer response status should be "200"
+
+  And the buyer array length should be greater the zero
--- features/step_definitions/LoginSteps.rb
+++ features/step_definitions/LoginSteps.rb
@@ -3,8 +3,8 @@ require "net/http"
 require 'openssl'
 require "rspec/expectations"
 
-
-
+access_token = ''
+customer_guid = ''
 Given /^I send credentials and send mark down body$/ do |credentials|
   url = URI("https://dev-8s97qw7o.auth0.com/oauth/token")
   
@@ -17,7 +17,9 @@ Given /^I send credentials and send mark down body$/ do |credentials|
   request.body = credentials
   
   @response = https.request(request)
-  puts @response.read_body
+  access_token = JSON.parse(@response.body)['access_token']
+  puts access_token
+ 
   
 end 
 
@@ -40,11 +42,14 @@ Given /^I send and accept JSON$/ do
   https.use_ssl = true
   https.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new(url)
-    request["Authorization"] = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJqRTJRVVZFTWpFME5URXlORUZCUTBFMU9UaEdRVEEwUVVRek1USkRRemd4T1VGRE5qQkJNUSJ9.eyJpc3MiOiJodHRwczovL2Rldi04czk3cXc3by5hdXRoMC5jb20vIiwic3ViIjoiRWxXM3NLbzZ3aHZBTTFVSE9FSTNXY2prREdaMXc1VDZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYmx1ZXZpc2lvbi5jb20iLCJpYXQiOjE2MDYxNDY3NDUsImV4cCI6MTYwODczODc0NSwiYXpwIjoiRWxXM3NLbzZ3aHZBTTFVSE9FSTNXY2prREdaMXc1VDYiLCJzY29wZSI6IioiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.JNf7pyUDJjd8rwF5mK7KT1EeseWZdNcDYZHWQzZw6sKEygORUoXa5IR_0Q2tv0roHM4Elv18jePBTLZb1c-a2CXu9voiwAJGn71dfQfnNK27He2xB9Hj29siKzcG5xphyxyuxHCPMHJbM-Xr6FNUVT3nXMBCr75-TIsl9shI4qLH-2YJeEmuRxaV3slOw1Dfqy_Tam91b5V-gjas50OR9-f4ZUO4BZnbo_8-T5Em24KtaUAI7NX1CsBjG0CeXGd3htN4EPeCzoA9h7GnbhvyNkzTU4l8bY7WD1Y_bSBKNsdv_BkxC2KjN6aGpQxbTdGbCG5Emp2vkn9N4ctv8tKYAw"
+    puts @access_token
+    request["Authorization"] = "Bearer " + access_token
     request["Content-Type"] = "application/json"
     request.body = "{\r\n  \"appUserId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\r\n  \"appUserName\": \"sqadri1166\",\r\n  \"appNameDesc\": \"string\",\r\n  \"appRoleId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\r\n  \"appUserPwd\": \"Astaghee@166\",\r\n  \"isActive\": true,\r\n  \"createdDate\": \"2020-11-18T14:47:38.636Z\",\r\n  \"createdBy\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\"\r\n}"
 
     @responseobj = https.request(request)
+    customer_guid =  JSON.parse(@responseobj.body)['customerTokenId']
+    puts customer_guid
     
 
 end
@@ -59,4 +64,38 @@ Then /^the response status should be "([^"]*)"$/ do |status|
   end
 end
 
+Given /^i send data for buyers information and accept  data in JSON$/ do
+  url = URI("https://localhost:44398/api/AppBuyer/GetAllBuyers")
 
+  https = Net::HTTP.new(url.host, url.port)
+  https.use_ssl = true
+  https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    
+    request["Authorization"] = "Bearer " + access_token
+    request["Content-Type"] = "application/json"
+    request['CustomerGuidKey'] = customer_guid 
+    @responseJ = https.request(request)
+    puts @responseJ
+    @buyers =    JSON.parse(@responseJ.body) 
+       
+
+end 
+
+Then /^the buyer response status should be "([^"]*)"$/ do |status|
+  begin
+    @responseJ.code.should eq(status)
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    puts "Response body:"
+    raise e
+  end
+end
+
+And /^the buyer array length should be greater the zero$/ do 
+  begin
+    JSON.parse(@responseJ.body).length.should eq(3) 
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    puts "Response body:" 
+    raise e
+  end
+end
